c46b06f31a7d892ffff3c913fbd404e717385af4
Raiden-Rayabuza <60820299+Raiden-Rayabuza@users.noreply.github.com>
2022-05-23 15:31:48 -0300

    Delete src directory

--- src/main/java/br/com/fateczl/p3_siga2/repository/NotasRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.fateczl.p3_siga2.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.fateczl.p3_siga2.model.Notas;
-
-public interface NotasRepository extends JpaRepository<Notas,String>{
-    
-}
